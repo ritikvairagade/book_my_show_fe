7b2dd9c343fadda2ad389e399053c2b7c17b34e0
ritikvairagade <ritikvairagade@gmail.com>
2021-07-17 13:44:10 +0530

    hoc and layouts

---
+++ planning.js
@@ -0,0 +1,26 @@
+// Book my show planning
+
+//--> we need reuseable carousel
+//   --> hero carousel
+//   --> common carousel
+// --> cast and crew small carouse
+
+
+
+// pages
+
+// --> home page
+// --> movie page
+// --> categories page 
+// --> auth page
+// --> razor payment gateway
+
+
+// Plan -> how are we building it?
+// layout
+// Routes
+// Pages
+// Components
+
+// HOC
+// higher order components
